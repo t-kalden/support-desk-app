3a66c3f4d53d675a2cd5911f21acad3063586681
t-kalden <kalden.t@outlook.com>
2023-07-31 15:31:52 -0400

    adding user registration logic, and creating user model for mongodb

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,49 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
 
 // @desc   Register a new user
 // @route  /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, isAdmin } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please inclue all fields");
+    throw new Error("Please include all fields");
+  }
+
+  //Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    isAdmin,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
   res.send("Register Route");
 });
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
