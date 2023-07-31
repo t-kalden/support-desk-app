440fe4b57336c5cafaf2e8070a081f1832a9338e
t-kalden <kalden.t@outlook.com>
2023-07-31 16:19:51 -0400

    protect routes and auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,6 +73,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Get current user
+// @route  /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(req.user);
+});
+
 // Generate web token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -83,4 +95,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
