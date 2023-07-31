ecea087586f805a56246bf05a38faebece84da1f
t-kalden <kalden.t@outlook.com>
2023-07-31 14:33:24 -0400

    adding db connectivity, routing tests, and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log("Server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+//CONNECT TO DB
+
+connectDB();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+//Error handling
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
