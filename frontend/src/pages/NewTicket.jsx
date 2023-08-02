015bdb9c87fe36629a7eb2e717cdb2cb0e31df82
t-kalden <kalden.t@outlook.com>
2023-08-02 17:39:25 -0400

    build create ticket form

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function NewTicket() {
-  return <div>NewTicket</div>;
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState("iPhone");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out form below for support</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Customer Email</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="product">Product</label>
+            <select
+              name="product"
+              id="product"
+              value={product}
+              onChange={(e) => {
+                setProduct(e.target.value);
+              }}
+            >
+              <option value="iPhone">iPhone</option>
+              <option value="MacBook Pro">MacBook Pro</option>
+              <option value="iMac">iMac</option>
+              <option value="iPad">iPad</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of the Issue</label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              value={description}
+              placeholder="Description"
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default NewTicket;
