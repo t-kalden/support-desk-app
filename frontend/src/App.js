7d20303ef402f84d09c8bd1b347b9f4de15289a0
t-kalden <kalden.t@outlook.com>
2023-08-02 17:02:22 -0400

    create new ticket page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NewTicket from "./pages/NewTicket";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/register" element={<Register />}></Route>
+            <Route path="/new-ticket" element={<NewTicket />}></Route>
           </Routes>
         </div>
       </Router>
---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,5 @@
+function NewTicket() {
+  return <div>NewTicket</div>;
+}
+
+export default NewTicket;
